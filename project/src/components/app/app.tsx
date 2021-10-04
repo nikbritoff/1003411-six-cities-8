cde7cc098f354d4b8f782392a1d3cd4b4dc2f34d
Nikita Britov <btitov.nd@gmail.com>
2021-10-04 19:05:35 +0300

    Удаление лишнего

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,7 @@
 import MainScreen from '../main-screen/main-screen';
 import { MainScreenProps } from '../../types/main-screen';
-import Favorites from '../favorites/favorites';
-// import Property from '../property/property';
 
 function App(props: MainScreenProps): JSX.Element {
-  // return Property();
-  return Favorites();
   return MainScreen(props);
 }
 
