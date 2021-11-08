0a7e3aa34790e0866e4cbb4c46e33f5483e40e48
Nikita Britov <btitov.nd@gmail.com>
2021-11-08 11:08:59 +0300

    Исправление меток карты

--- project/src/const.ts
+++ project/src/const.ts
@@ -83,8 +83,5 @@ export const CITIES = [
   },
 ];
 
-
-export const URL_MARKER_DEFAULT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
-export const URL_MARKER_CURRENT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
-// export const URL_MARKER_DEFAULT = '../public/img/pin.svg';
-// export const URL_MARKER_CURRENT = '../public/img/pin-active.svg';
+export const URL_MARKER_DEFAULT = './img/pin.svg';
+export const URL_MARKER_CURRENT = './img/pin-active.svg';
